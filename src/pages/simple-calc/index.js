64b35b3408f74a7c11bfa1b00ee4cd686f9e3328
Hyun Don Moon <hyundonmoon@gmail.com>
2022-01-25 22:41:23 +0900

    add: 환율 및 수취 금액 올바른 형식 적용, 하드코딩된 데이터 상수로 변환 [#3]

--- src/pages/simple-calc/index.js
+++ src/pages/simple-calc/index.js
@@ -1,11 +1,104 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
-const currencyList = [
+// styled components
+const StyledContainer = styled.main`
+  flex: 1 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  min-width: 20rem;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: center;
+  }
+
+  & > *:not(:first-child):not(:last-child) {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &>*: not(: first-child) {
+    margin-top: 1rem;
+  }
+`;
+
+const StyledButton = styled.button`
+  background: #02ce89;
+  color: #fff;
+  font-weight: bold;
+  border: 1px solid #02ce89;
+  padding: 4px 0px;
+  border-radius: 50px;
+`;
+
+const StyledTitle = styled.h1`
+  color: #02ce89;
+  font-weight: bold;
+  padding: 4px 0px;
+  margin: 12px;
+`;
+
+const StyledSelect = styled.select`
+  background: #02ce89;
+  color: #fff;
+  padding: 4px 0;
+  font-size: 16px;
+  border-radius: 5px;
+`;
+
+const StyledInput = styled.input`
+  border: 1px solid #d3d7e1;
+  background: #fff;
+  border-radius: 50px;
+  padding: 4px;
+  text-align: center;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+`;
+
+// constants
+const CURRENCY_LIST = [
   { currency: 'KRW', country: '한국' },
   { currency: 'JPY', country: '일본' },
   { currency: 'PHP', country: '필리핀' },
 ];
+const CALCULATOR_TITLE = '환율 계산';
+const SENDER_LABEL = '송금국가';
+const SENDER_COUNTRY = '미국';
+const SENDER_CURRENCY = 'USD';
+const RECIPIENT_LABEL = '수취국가';
+const CURRENCY_LABEL = '환율';
+const ERROR_MESSAGE = '송금액이 바르지 않습니다';
+const API_LINK = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_CURRENCY_KEY}`;
+
+const formatNumber = num => {
+  if (typeof num !== 'number') return null;
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+  })
+    .format(num)
+    .slice(1);
+};
+
+const convertStrToNum = str => {
+  const replaced = str.replace(/,/g, '');
+  return parseFloat(replaced).toFixed(2);
+};
 
 function SimpleCalc() {
   const [currency, setCurrency] = useState('KRW');
@@ -17,11 +110,10 @@ function SimpleCalc() {
 
   useEffect(() => {
     axios
-      .get(
-        `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_CURRENCY_KEY}`,
-      )
+      .get(API_LINK)
       .then(res => {
-        setRate(res.data.quotes[`USD${currency}`]);
+        const data = res.data.quotes[`USD${currency}`];
+        setRate(formatNumber(data));
       })
       .catch();
   }, [currency]);
@@ -31,6 +123,7 @@ function SimpleCalc() {
   };
 
   const handleChange = e => {
+    if (showMessage) setShowMessage(prev => !prev);
     setAmount(e.target.value);
   };
 
@@ -40,7 +133,8 @@ function SimpleCalc() {
       amount < 0 ||
       amount > 10000 ||
       amount === undefined ||
-      amount === null
+      amount === null ||
+      amount === ''
     ) {
       setShowMessage(true);
       setIsWrong(true);
@@ -48,44 +142,62 @@ function SimpleCalc() {
     } else {
       setShowMessage(true);
       setIsWrong(false);
-      setResult(rate * amount);
+      setResult(convertStrToNum(rate) * amount);
     }
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <h1>환율 계산</h1>
-        <p>송금국가: 미국(USD)</p>
-        <label htmlFor="currency-box">
-          수취국가:
-          <select name="currency" id="currency-box" onChange={handleSelect}>
-            {currencyList.map(({ currency: c, country }) => (
-              <option value={c} key={c}>{`${country}(${c})`}</option>
-            ))}
-          </select>
-        </label>
-        <p>
-          환율: {rate} {currency}/USD
-        </p>
-        <label htmlFor="amount-box">
-          송금액:
-          <input
-            type="number"
-            name="amount-box"
-            id="amount-box"
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      {showMessage && isWrong && <p>송금액이 바르지 않습니다</p>}
-      {showMessage && !isWrong && (
-        <p>
-          수취금액은 {result} {currency}입니다.
-        </p>
-      )}
-    </>
+    <StyledContainer>
+      <div>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledTitle>{CALCULATOR_TITLE}</StyledTitle>
+          <p>
+            <span>{SENDER_LABEL}:</span>
+            <span>
+              {SENDER_COUNTRY} ({SENDER_CURRENCY})
+            </span>
+          </p>
+          <label htmlFor="currency-box">
+            {RECIPIENT_LABEL}:
+            <StyledSelect
+              name="currency"
+              id="currency-box"
+              onChange={handleSelect}>
+              {CURRENCY_LIST.map(({ currency: c, country }) => (
+                <option value={c} key={c}>{`${country}(${c})`}</option>
+              ))}
+            </StyledSelect>
+          </label>
+          <p>
+            <span>{CURRENCY_LABEL}:</span>
+            <span>
+              {rate} {currency}/{SENDER_CURRENCY}
+            </span>
+          </p>
+          <label htmlFor="amount-box">
+            송금액:
+            <span>
+              <StyledInput
+                type="number"
+                name="amount-box"
+                id="amount-box"
+                onChange={handleChange}
+              />
+              {SENDER_CURRENCY}
+            </span>
+          </label>
+          <StyledButton type="submit">Submit</StyledButton>
+        </StyledForm>
+        {showMessage &&
+          (isWrong ? (
+            <ErrorMessage>{ERROR_MESSAGE}</ErrorMessage>
+          ) : (
+            <p>
+              수취금액은 {formatNumber(result)} {currency}입니다.
+            </p>
+          ))}
+      </div>
+    </StyledContainer>
   );
 }
 
